680f82fc8d5d01be7a83ab762c4e1942ae93a620
JeremyColegrove2U <48567475+JeremyMColegrove@users.noreply.github.com>
2022-04-24 19:19:24 -0500

    Connected to live mongodb site

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://jcolegrove2u:85cCEszjv6s49smX@googlebooks.xilsy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
